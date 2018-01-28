9c3a7a763c2952b983d684648b9fb1195b8aa59e
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2018-01-28 14:39:20 -0800

    list notes under each category

--- app/ClientApp/app/components/notes/notes.component.ts
+++ app/ClientApp/app/components/notes/notes.component.ts
@@ -1,5 +1,7 @@
 import { Component,Inject } from '@angular/core';
-import {Http} from '@angular/http';   
+import {Http} from '@angular/http'
+import {NgModule} from '@angular/core'
+import {FormsModule} from '@angular/forms'
 
 @Component({
     selector: 'notes',
@@ -9,34 +11,38 @@ import {Http} from '@angular/http';
 export class NotesComponent {
 
     public notes: Note[];
+    public categories: Category[];
     public count: number;
+    public selectedNote: Note;
 
     constructor( http: Http, @Inject('BASE_URL') baseUrl: string){
+
         http.get(baseUrl + 'api/notes/notes').subscribe(result => {
             this.notes= result.json() as Note[];
             this.count=this.notes.length;
-            console.log('test');
-        }, error=> console.log('error'));
+            console.log('getting notes');
+        }, error=> console.log('notes error'));
 
-        
+
+        http.get(baseUrl + 'api/notes/categories').subscribe(result =>{
+          this.categories=result.json() as Category[];
+          console.log('getting categories');
+        }, error=>console.log('categories error'));
+
+        console.log('calling constructor');
     }
 
 
-
-/*
-    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-        http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-            this.forecasts = result.json() as WeatherForecast[];
-        }, error => console.error(error));
+    onSelect(note: Note): void{
+      console.log('selected note: note.text');
+      this.selectedNote=note;
     }
-
-*/
-
-   
-    
 }
 
-
+interface Category{
+  categoryId: number;
+  name: string;
+}
 
 interface Note{
     commentId: number;
@@ -44,5 +50,5 @@ interface Note{
     text: string;
     updateUser: string;
 
-}
 
+}
