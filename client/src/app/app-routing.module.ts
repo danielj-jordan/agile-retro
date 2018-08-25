f434f2ab9fd9108fb41ef560426866504b503b5a
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2018-08-24 18:06:44 -0700

    create very basic login page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotesComponent } from './components/notes/notes.component';
+import { LoginComponent } from './components/login/login.component';
+import { RetrospectivelistComponent } from './components/retrospectivelist/retrospectivelist.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch:'full' },
   { path: 'notes', component: NotesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'list',  component: RetrospectivelistComponent}
 ];
 
 @NgModule({
