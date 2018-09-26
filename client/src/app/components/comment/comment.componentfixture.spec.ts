43e5e9ab6593cc993dc190a1ec8ac40b258c6726
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2018-09-25 20:53:36 -0700

    fix linter errors in comment component

--- client/src/app/components/comment/comment.componentfixture.spec.ts
+++ client/src/app/components/comment/comment.componentfixture.spec.ts
@@ -1,136 +1,111 @@
 import { assert } from 'chai';
 import { CommentComponent } from './comment.component';
 import { TestBed, async, inject, ComponentFixture } from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NotesService } from '../../services/notes.service';
 import { NotesServiceMock } from '../../services/notes.service.mock';
-import {CommentEditComponent} from '../commentedit/commentedit.component';
-import {Comment} from '../../models/comment';
-import {Category} from '../../models/category';
+import { CommentEditComponent } from '../commentedit/commentedit.component';
+import { Comment } from '../../models/comment';
+import { Category } from '../../models/category';
 import { FormsModule } from '@angular/forms';
-import {Observable} from 'rxjs/Rx';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
 
-
-
-
-
 describe('comment component fixture', () => {
-    let commentComponent: CommentComponent;
-    let noteService: NotesService;
-    let fixture: ComponentFixture<CommentComponent>;
-    let component: CommentComponent;
-    
-    beforeEach(() => {
-        TestBed.configureTestingModule({ 
-            declarations: [CommentComponent, CommentEditComponent],
-            imports:[FormsModule, RouterTestingModule],
-            providers: [{provide: NotesService, useValue: new NotesServiceMock() }]
-        
+  let commentComponent: CommentComponent;
+  let fixture: ComponentFixture<CommentComponent>;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CommentComponent, CommentEditComponent],
+      imports: [FormsModule, RouterTestingModule],
+      providers: [{ provide: NotesService, useValue: new NotesServiceMock() }]
     }).compileComponents();
 
     jasmine.clock().install();
-   // jasmine.DEFAULT_TIMEOUT_INTERVAL=20000;
-    TestBed.overrideProvider(NotesService, {useValue: new NotesServiceMock() });
+    TestBed.overrideProvider(NotesService, { useValue: new NotesServiceMock() });
     fixture = TestBed.createComponent(CommentComponent);
     fixture.debugElement.injector.get(NotesService);
     commentComponent = fixture.componentInstance;
-   // commentComponent.cancelTimer();
- 
-    
-    
-          
-    });
+  });
 
-    afterEach(()=>{
-        jasmine.clock().uninstall();
-    })
+  afterEach(() => {
+    jasmine.clock().uninstall();
+  });
 
-    it('meeting name is displayed says test', () =>{
-        fixture.detectChanges();
+  it('meeting name is displayed says test', () => {
+    fixture.detectChanges();
+    const banner: HTMLElement = fixture.nativeElement.querySelector('h3');
+    expect(banner.textContent).toEqual('test');
+  });
 
-        const banner: HTMLElement=fixture.nativeElement.querySelector("h3");
-        expect(banner.textContent).toEqual("test");
+   it('because the service is mocked, there are two categories', () => {
+    fixture.detectChanges();
 
-    });
+    const category1 = fixture.nativeElement.querySelector('#category1');
+    const category2 = fixture.nativeElement.querySelector('#category2');
+    const category3 = fixture.nativeElement.querySelector('#category3');
 
-    it('because the service is mocked, there are two categories', () =>{
-        fixture.detectChanges();
+    expect(category1).toBeDefined();
+    expect(category2).toBeDefined();
+    expect(category3).toBeNull();
+  });
 
-        const category1 = fixture.nativeElement.querySelector("#category1");
-        const category2 = fixture.nativeElement.querySelector("#category2");
-        const category3 = fixture.nativeElement.querySelector("#category3");
+  it('because the service is mocked, there are two comment in category 1', () => {
+    fixture.detectChanges();
 
-        expect(category1).toBeDefined();
-        expect(category2).toBeDefined();
-        expect(category3).toBeNull();
-    });
+    const note1 = fixture.nativeElement.querySelector('#comment1');
+    const note2 = fixture.nativeElement.querySelector('#comment2');
+    const note3 = fixture.nativeElement.querySelector('#comment3');
 
-    it('because the service is mocked, there are two comment in category 1', () =>{
-        fixture.detectChanges();
-
-        //todo: still need to check for category
-
-        const note1 = fixture.nativeElement.querySelector("#comment1");
-        const note2 = fixture.nativeElement.querySelector("#comment2");
-        const note3 = fixture.nativeElement.querySelector("#comment3");
-
-        expect(note1).toBeDefined();
-        expect(note2).toBeDefined();
-        expect(note3).toBeNull();
-    
-    });
+    expect(note1).toBeDefined();
+    expect(note2).toBeDefined();
+    expect(note3).toBeNull();
+  });
 
 
-    it('move mouse over note to show footer with edit and delete buttons', () =>{
+  it('move mouse over note to show footer with edit and delete buttons', () => {
 
-      fixture.detectChanges();
-      const nativeElement: HTMLElement= fixture.nativeElement.querySelector('#comment1');
-      var note= fixture.nativeElement.querySelector('#comment1');
+    fixture.detectChanges();
+    const nativeElement: HTMLElement = fixture.nativeElement.querySelector('#comment1');
+    const note = fixture.nativeElement.querySelector('#comment1');
 
-      expect(note.querySelector(".card-footer").style.display).toBe('none');
-       
-      note.dispatchEvent(new MouseEvent('mouseenter', {
-        view: window,
-        bubbles: true,
-        cancelable: true
-      })); 
+    expect(note.querySelector('.card-footer').style.display).toBe('none');
 
-      fixture.detectChanges();
-      expect(note.querySelector(".card-footer").style.display).toBe('');        
-    });
+    note.dispatchEvent(new MouseEvent('mouseenter', {
+      view: window,
+      bubbles: true,
+      cancelable: true
+    }));
 
-  
-
-    it('clicking the delete button removes it from the collection', () => {
-        
-        spyOn(commentComponent,"onDeleteId");
-        fixture.detectChanges();
-        const note1 = fixture.nativeElement.querySelector("#comment1");
-        const note1DeleteButton= note1.querySelector(".retro-btn-delete");
-        fixture.detectChanges();
-        note1DeleteButton.dispatchEvent(new Event('click'));
-        fixture.detectChanges();
-    
-        expect(commentComponent.onDeleteId).toHaveBeenCalled();
-    });
-    
-    it('clicking the edit button displays the edit note component', () => {
-        spyOn(commentComponent,"onEditId");
-        fixture.detectChanges();
-        const note1 = fixture.nativeElement.querySelector("#comment1");
-        const note1EditButton= note1.querySelector(".retro-btn-edit");
-        fixture.detectChanges();
-        note1EditButton.dispatchEvent(new Event('click'));
-        fixture.detectChanges();
-    
-        expect(commentComponent.onEditId).toHaveBeenCalled();
+    fixture.detectChanges();
+    expect(note.querySelector('.card-footer').style.display).toBe('');
+  });
 
 
-    });
+  it('clicking the delete button removes it from the collection', () => {
+    spyOn(commentComponent, 'onDeleteId');
+    fixture.detectChanges();
+    const note1 = fixture.nativeElement.querySelector('#comment1');
+    const note1DeleteButton = note1.querySelector('.retro-btn-delete');
+    fixture.detectChanges();
+    note1DeleteButton.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
 
+    expect(commentComponent.onDeleteId).toHaveBeenCalled();
+  });
+
+  it('clicking the edit button displays the edit note component', () => {
+    spyOn(commentComponent, 'onEditId');
+    fixture.detectChanges();
+    const note1 = fixture.nativeElement.querySelector('#comment1');
+    const note1EditButton = note1.querySelector('.retro-btn-edit');
+    fixture.detectChanges();
+    note1EditButton.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+
+    expect(commentComponent.onEditId).toHaveBeenCalled();
+  });
 
 });
