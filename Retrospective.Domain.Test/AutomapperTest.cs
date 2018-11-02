71dc4b4f0f586522376457ad19697345d0e002d3
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2018-11-01 17:21:42 -0700

    implement meetingmanager

--- Retrospective.Domain.Test/AutomapperTest.cs
+++ Retrospective.Domain.Test/AutomapperTest.cs
@@ -1,30 +1,20 @@
-using Xunit;
 using AutoMapper;
+using Xunit;
 
-namespace Retrospective.Domain.Test
-{
-    public class AutomapperTest
-    {
-        private IMapper mapper; 
+namespace Retrospective.Domain.Test {
+    public class AutomapperTest {
         private MapperConfiguration config;
 
-        public AutomapperTest()
-        {
-            config = new MapperConfiguration(c =>
-            {
-                c.AddProfile<Retrospective.Domain.DomainProfile>();
+        public AutomapperTest () {
+            config = new MapperConfiguration (c => {
+                c.AddProfile<Retrospective.Domain.DomainProfile> ();
             });
-        
+
         }
 
         [Fact]
-        public void AutomapperConfiguration()
-        {
-
-            config.AssertConfigurationIsValid();
-        
+        public void AutomapperConfiguration () {
+            config.AssertConfigurationIsValid ();
         }
-
-
     }
 }
