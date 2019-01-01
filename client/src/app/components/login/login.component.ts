f93aefeffbb9bcafc55fd9baef1c490236916aba
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2019-01-01 07:18:10 -0800

    force logout when visiting login screen

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {FormsModule} from '@angular/forms'
-import {User} from '../../models/user';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'
+import { User } from '../../models/user';
 import { LocalstorageService } from '../../services/localstorage.service';
-import {NotesService} from '../../services/notes.service';
+import { NotesService } from '../../services/notes.service';
 import { UserLogin } from '../../models/userlogin';
 import { environment } from '../../../environments/environment';
 
@@ -16,11 +16,11 @@ import { environment } from '../../../environments/environment';
 export class LoginComponent implements OnInit {
 
   constructor(private router: Router,
-              private storage: LocalstorageService,
-              private notesService: NotesService) { 
+    private storage: LocalstorageService,
+    private notesService: NotesService) {
 
-              }
-  public  model = new User();
+  }
+  public model = new User();
 
   public isDemo: boolean;
 
@@ -28,24 +28,24 @@ export class LoginComponent implements OnInit {
 
 
   ngOnInit() {
-    this.isDemo=environment.demoEnabled;
-    this.isLoginAllowed=environment.signupEnabled;
-
-
+    this.isDemo = environment.demoEnabled;
+    this.isLoginAllowed = environment.signupEnabled;
+    this.storage.user = null;
+    this.storage.userToken = null;
   }
 
-  
 
-  public LoginDemo():void{
+
+  public LoginDemo(): void {
 
     this.notesService.loginDemo().subscribe(
-      data=>{
+      data => {
         console.log('test');
         console.log(data);
-        this.storage.userToken=data.token;
+        this.storage.userToken = data.token;
 
         // save to local storage
-        this.storage.user=this.model;
+        this.storage.user = this.model;
 
         // redirect to teams retrospectivelist page
         this.router.navigateByUrl('/retrospective/list');
@@ -54,29 +54,29 @@ export class LoginComponent implements OnInit {
   }
 
 
-  public Login():void{
-    console.log('logging in user: ' + this.model.loginName);  
+  public Login(): void {
+    console.log('logging in user: ' + this.model.loginName);
 
-    if(!this.isLoginAllowed){
+    if (!this.isLoginAllowed) {
       return;
     }
-    
-    
+
+
     //verify login -- assume valid for now
     let login = new UserLogin();
-    login.LoginName=this.model.loginName;
+    login.LoginName = this.model.loginName;
 
 
     this.notesService.login(login).subscribe(
-      data=>{
+      data => {
         console.log('test');
         console.log(data);
-        this.storage.userToken=data.token;
+        this.storage.userToken = data.token;
 
-        
+
 
         // save to local storage
-        this.storage.user=this.model;
+        this.storage.user = this.model;
 
         // redirect to teams retrospectivelist page
         this.router.navigateByUrl('/retrospective/list');
