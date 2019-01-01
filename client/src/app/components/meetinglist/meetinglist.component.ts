282fbe567a35b6bd4dad6ba1f461e6e73ef49d05
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2019-01-01 12:07:36 -0800

    refactor layout on meeting list component

--- client/src/app/components/meetinglist/meetinglist.component.ts
+++ client/src/app/components/meetinglist/meetinglist.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { NotesService } from '../../services/notes.service';
 import { LocalstorageService } from '../../services/localstorage.service';
-import {User} from '../../models/user';
-import {Team} from '../../models/team';
+import { User } from '../../models/user';
+import { Team } from '../../models/team';
 
 
 @Component({
@@ -16,7 +16,7 @@ export class MeetingListComponent implements OnInit {
   constructor(
     private router: Router,
     private storage: LocalstorageService,
-    private noteService: NotesService) { 
+    private noteService: NotesService) {
   }
 
   public teams: Team[];
@@ -25,21 +25,55 @@ export class MeetingListComponent implements OnInit {
   ngOnInit() {
 
 
-       console.log('getting teams');
-       
-       //get the teams for this user
-       this.noteService.getUserTeams().subscribe(
-         data=>{
+    console.log('getting teams');
 
-          console.log(data);
-          this.teams=data;
-          console.log('user teams: ' + this.teams);
-          //get the retrospective meeting sessions for each team
-          for(let team of this.teams){
-            this.noteService.getTeamMeetings(team.teamId).subscribe(
-              data=>team.meetings=data);
-          }
-          console.log(this.teams);
-       });
-      }
+    //get the teams for this user
+    this.noteService.getUserTeams().subscribe(
+      data => {
+
+        console.log(data);
+        this.teams = data;
+        console.log('user teams: ' + this.teams);
+        //get the retrospective meeting sessions for each team
+        for (let team of this.teams) {
+          this.noteService.getTeamMeetings(team.teamId).subscribe(
+            data => {
+              team.meetings = data;
+
+              //assemble summary of team
+              team.message = `This team has ${team.members.length} member`;
+              if (team.members.length != 1) {
+                team.message += 's';
+              }
+              team.message += ` and ${data.length} retrospective meeting`;
+              if (data.length != 1) {
+                team.message += 's.';
+              } else {
+                team.message += '.';
+              }
+
+              for(let meeting of data){
+                meeting.message=`This retrospective meeting has ${meeting.categories.length} `;
+                if(meeting.categories.length!=0){
+                  meeting.message += 'categories'
+                }else{
+                  meeting.message+= 'category'
+                }
+                
+                this.noteService.getNotes(meeting.id).subscribe(
+                  meetingData=>{
+                    meeting.message += ` and ${meetingData.length}`;
+                    if(meetingData.length !=1){
+                      meeting.message += ' comments';
+                    }else{
+                      meeting.message += ' comment';
+                    }
+                  }
+                )
+              }
+            });
+        }
+        console.log(this.teams);
+      });
+  }
 }
