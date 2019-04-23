a2d1e9f0a6af4f8225b15213d510f2f25f13b3ae
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2019-04-22 20:49:15 -0700

    add privacy policy page

--- web/client/src/app/app-routing.module.ts
+++ web/client/src/app/app-routing.module.ts
@@ -3,22 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommentComponent } from './components/comment/comment.component';
 import { LoginComponent } from './components/login/login.component';
 import { MeetingListComponent } from './components/meetinglist/meetinglist.component';
-import {MeetingEditComponent } from './components/meetingedit/meetingedit.component';
+import { MeetingEditComponent } from './components/meetingedit/meetingedit.component';
 import { TeamEditComponent } from './components/teamedit/teamedit.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { PrivacyPolicyComponent } from './components/privacypolicy/privacypolicy.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch:'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'retrospective',
+  {
+    path: 'retrospective',
     component: MenuComponent,
     children: [
-      { path: 'comment/:id', component: CommentComponent}, 
-      { path: 'meetingedit/:teamid/:meetingid', component: MeetingEditComponent},
-      { path: 'meetingedit/:teamid', component: MeetingEditComponent},
-      { path: 'teamedit/:teamid', component: TeamEditComponent},
-      { path: 'teamedit', component: TeamEditComponent},
-      { path: 'list',  component: MeetingListComponent}
+      { path: 'comment/:id', component: CommentComponent },
+      { path: 'meetingedit/:teamid/:meetingid', component: MeetingEditComponent },
+      { path: 'meetingedit/:teamid', component: MeetingEditComponent },
+      { path: 'teamedit/:teamid', component: TeamEditComponent },
+      { path: 'teamedit', component: TeamEditComponent },
+      { path: 'list', component: MeetingListComponent },
+      { path: 'privacypolicy', component: PrivacyPolicyComponent }
     ]
   }
 ];
@@ -26,10 +29,10 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      onSameUrlNavigation:'reload'
+      onSameUrlNavigation: 'reload'
     }),
   ],
-  exports: [ RouterModule ]
+  exports: [RouterModule]
 
 
 })
