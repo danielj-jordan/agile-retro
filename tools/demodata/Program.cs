ab65caccbff97572958f34ae9079c78366d77f13
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2019-06-24 19:03:28 -0700

    command line program to reset demo data

---
+++ tools/demodata/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Driver;
+using Retrospective.Data;
+
+namespace demodata
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTIONSTRING");
+      var databaseName = Environment.GetEnvironmentVariable("DB_NAME");
+
+      //start with an empty database
+      var client = new MongoClient(connectionString);
+
+      //connect to the database
+      Retrospective.Data.Database database = new Database(databaseName);
+
+      DemoData demoData = new DemoData(database);
+      demoData.Initialize();
+    }
+  }
+}
