63ffae0fea607b8c34367924032c8427c4362950
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2019-07-06 15:17:14 -0700

    userid in jwt claim instead of email

--- apptest/TeamControllerTest.cs
+++ apptest/TeamControllerTest.cs
@@ -12,107 +12,118 @@ using Retrospective.Data.Model;
 using Retrospective.Domain;
 using Xunit;
 
-namespace apptest {
+namespace apptest
+{
 
-    [Collection ("Controller Test collection")]
-    public class TeamControllerTest {
+  [Collection("Controller Test collection")]
+  public class TeamControllerTest
+  {
 
-        AutoMapper.Mapper mapper = null;
-        TestFixture fixture;
+    AutoMapper.Mapper mapper = null;
+    TestFixture fixture;
 
-        TeamManager teamManager;
+    TeamManager teamManager;
 
-        public TeamControllerTest (TestFixture fixture) {
-            this.fixture = fixture;
-            var config = new MapperConfiguration (c => {
-                c.AddProfile<app.Domain.DomainProfile> ();
-                c.AddProfile<Retrospective.Domain.DomainProfile> ();
-            });
+    public TeamControllerTest(TestFixture fixture)
+    {
+      this.fixture = fixture;
+      var config = new MapperConfiguration(c =>
+      {
+        c.AddProfile<app.Domain.DomainProfile>();
+        c.AddProfile<Retrospective.Domain.DomainProfile>();
+      });
 
-            mapper = new Mapper (config);
+      mapper = new Mapper(config);
 
-            var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<TeamManager> ();
+      var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<TeamManager>();
 
-            teamManager = new TeamManager (logger, mapper, fixture.Database);
-        }
+      teamManager = new TeamManager(logger, mapper, fixture.Database);
+    }
 
-        private void MockHttpContextValid (app.Controllers.TeamController controller) {
-            controller.ControllerContext = new ControllerContext ();
-            controller.ControllerContext.HttpContext = new DefaultHttpContext ();
-            controller.ControllerContext.HttpContext.User = new ClaimsPrincipal (new ClaimsIdentity (new Claim[] {
-                new Claim (ClaimTypes.Name, fixture.Owner.Id.ToString())
-            }, "someAuthTypeName"));
-
-        }
-
-        [Fact]
-        public void GetTeamMembers () {
-
-            string teamId = fixture.TeamId.ToString ();
-
-            var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<app.Controllers.TeamController> ();
-
-            var controller = new app.Controllers.TeamController (logger, mapper, teamManager);
-
-            MockHttpContextValid (controller);
-
-            var teamMembers = controller.TeamMembers (teamId);
-
-            Assert.True (teamMembers.ToList ().Count () > 0);
-
-        }
-
-        [Fact]
-        public void GetUserTeams () {
-            var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<app.Controllers.TeamController> ();
-
-            var controller = new app.Controllers.TeamController (logger, mapper, teamManager);
-
-            MockHttpContextValid (controller);
-            var teams = controller.Teams ();
-
-            Assert.True (teams.Value.ToList ().Count () > 0);
-
-        }
-
-        [Fact]
-        public void SaveTeam () {
-
-            var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<app.Controllers.TeamController> ();
-
-            var controller = new app.Controllers.TeamController (logger, mapper,  teamManager);
-
-            app.Model.Team team = new app.Model.Team ();
-            team.Name = "test team";
-
-            MockHttpContextValid (controller);
-            var result = controller.Team (team);
-
-            Assert.True (!String.IsNullOrEmpty (result.Value.TeamId));
-
-        }
-
-        [Fact]
-        public void UpdateTeam () {
-
-            var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<app.Controllers.TeamController> ();
-
-            var controller = new app.Controllers.TeamController (logger, mapper,  teamManager);
-
-            MockHttpContextValid (controller);
-
-            var teamStart = mapper.DefaultContext.Mapper.Map<Retrospective.Domain.Model.Team,app.Model.Team>(
-                mapper.DefaultContext.Mapper.Map<Retrospective.Data.Model.Team,Retrospective.Domain.Model.Team>(fixture.TestTeam));
-
-            teamStart.Name += " more";
-
-            var teamEnd = controller.Team(teamStart);
-
-            Assert.Equal (teamStart.TeamId.ToString(), teamEnd.Value.TeamId);
-
-            Assert.Contains ("more", teamEnd.Value.Name);
-
-        }
+    private void MockHttpContextValid(app.Controllers.TeamController controller, User user)
+    {
+      controller.ControllerContext = new ControllerContext();
+      controller.ControllerContext.HttpContext = new DefaultHttpContext();
+      controller.ControllerContext.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] 
+      {
+        new Claim(ClaimTypes.Name, user.Name),
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+      }, "someAuthTypeName"));
 
     }
+
+    [Fact]
+    public void GetTeamMembers()
+    {
+
+      string teamId = fixture.TeamId.ToString();
+
+      var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<app.Controllers.TeamController>();
+
+      var controller = new app.Controllers.TeamController(logger, mapper, teamManager);
+
+      MockHttpContextValid(controller, fixture.Owner);
+
+      var teamMembers = controller.TeamMembers(teamId);
+
+      Assert.True(teamMembers.ToList().Count() > 0);
+
+    }
+
+    [Fact]
+    public void GetUserTeams()
+    {
+      var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<app.Controllers.TeamController>();
+
+      var controller = new app.Controllers.TeamController(logger, mapper, teamManager);
+
+      MockHttpContextValid(controller, fixture.Owner);
+      var teams = controller.Teams();
+
+      Assert.True(teams.Value.ToList().Count() > 0);
+
+    }
+
+    [Fact]
+    public void SaveTeam()
+    {
+
+      var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<app.Controllers.TeamController>();
+
+      var controller = new app.Controllers.TeamController(logger, mapper, teamManager);
+
+      app.Model.Team team = new app.Model.Team();
+      team.Name = "test team";
+
+      MockHttpContextValid(controller, fixture.Owner);
+      var result = controller.Team(team);
+
+      Assert.True(!String.IsNullOrEmpty(result.Value.TeamId));
+
+    }
+
+    [Fact]
+    public void UpdateTeam()
+    {
+
+      var logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<app.Controllers.TeamController>();
+
+      var controller = new app.Controllers.TeamController(logger, mapper, teamManager);
+
+      MockHttpContextValid(controller, fixture.Owner);
+
+      var teamStart = mapper.DefaultContext.Mapper.Map<Retrospective.Domain.Model.Team, app.Model.Team>(
+          mapper.DefaultContext.Mapper.Map<Retrospective.Data.Model.Team, Retrospective.Domain.Model.Team>(fixture.TestTeam));
+
+      teamStart.Name += " more";
+
+      var teamEnd = controller.Team(teamStart);
+
+      Assert.Equal(teamStart.TeamId.ToString(), teamEnd.Value.TeamId);
+
+      Assert.Contains("more", teamEnd.Value.Name);
+
+    }
+
+  }
 }
