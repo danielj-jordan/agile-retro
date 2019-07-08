8f28db0a4b40a2b370bd17d69ecd15a1fe9d6699
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2019-07-07 17:12:13 -0700

    fix compilation errors and add logging to demodata program

--- Retrospective.Data/Data/DataMeeting.cs
+++ Retrospective.Data/Data/DataMeeting.cs
@@ -8,82 +8,91 @@ using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 using Retrospective.Data.Model;
 
-namespace Retrospective.Data {
+namespace Retrospective.Data
+{
 
-    public class DataMeeting : IDataMeeting{
-        private string collection = "meeting";
+  public class DataMeeting : IDataMeeting
+  {
+    private string collection = "meeting";
 
-        private IDatabase database;
+    private IDatabase database;
 
-        public DataMeeting (IDatabase database) {
-            this.database = database;
+    public DataMeeting(IDatabase database)
+    {
+      this.database = database;
 
-        }
-
-        /// <summary>
-        /// Save a single retrospective object
-        /// </summary>
-        /// <param name="retrospective"></param>
-        /// <returns></returns>
-        public Meeting Save (Meeting meeting) {
-
-            Console.WriteLine("saving");
-
-            if (meeting.Id is null) {
-                database.MongoDatabase.GetCollection<Meeting> (collection).InsertOne (meeting);
-                
-            } else {
-                var filter = MongoDB.Driver.Builders<Meeting>.Filter.Eq ("Id", meeting.Id);
-                var saved = database.MongoDatabase.GetCollection<Meeting> (collection).ReplaceOne (filter, meeting);
-
-    
-             }
-
-            return meeting;
-        }
-
-
-        public List<Meeting> GetMeetings (string teamObjectId) {
-            return  this.GetMeetings(new ObjectId(teamObjectId));
-        
-        }
-        
-
-        /// <summary>
-        /// Get all Retrospective objects for a given team
-        /// </summary>
-        /// <param name="teamObjectId"></param>
-        /// <returns></returns>
-        public List<Meeting> GetMeetings (ObjectId teamObjectId) {
-            var sessions = database.MongoDatabase.GetCollection<Meeting> (collection);
-            var query = from session in sessions.AsQueryable<Meeting> ()
-            where session.TeamId == teamObjectId
-            select session;
-
-            return query.ToList<Meeting> ();
-
-        }
-
-
-       public Meeting Get (string meetingId) {
-            return  this.Get(new ObjectId(meetingId));
-        
-        }
-
-                /// <summary>
-        /// Get all Retrospective objects for a given team
-        /// </summary>
-        /// <param name="teamObjectId"></param>
-        /// <returns></returns>
-        public Meeting Get (ObjectId retroId) {
-            var sessions = database.MongoDatabase.GetCollection<Meeting> (collection);
-            var query = from session in sessions.AsQueryable<Meeting> ()
-            where session.Id == retroId
-            select session;
-
-            return query.FirstOrDefault();
-
-        }
     }
+
+    /// <summary>
+    /// Save a single retrospective object
+    /// </summary>
+    /// <param name="retrospective"></param>
+    /// <returns></returns>
+    public Meeting Save(Meeting meeting)
+    {
+
+      if (meeting.Id is null)
+      {
+        database.MongoDatabase.GetCollection<Meeting>(collection).InsertOne(meeting);
+
+      }
+      else
+      {
+        var filter = MongoDB.Driver.Builders<Meeting>.Filter.Eq("Id", meeting.Id);
+        var saved = database.MongoDatabase.GetCollection<Meeting>(collection).ReplaceOne(filter, meeting);
+
+
+      }
+
+      return meeting;
+    }
+
+
+    public List<Meeting> GetMeetings(string teamObjectId)
+    {
+      return this.GetMeetings(new ObjectId(teamObjectId));
+
+    }
+
+
+    /// <summary>
+    /// Get all Retrospective objects for a given team
+    /// </summary>
+    /// <param name="teamObjectId"></param>
+    /// <returns></returns>
+    public List<Meeting> GetMeetings(ObjectId teamObjectId)
+    {
+      var sessions = database.MongoDatabase.GetCollection<Meeting>(collection);
+      var query = from session in sessions.AsQueryable<Meeting>()
+                  where session.TeamId == teamObjectId
+                  select session;
+
+      return query.ToList<Meeting>();
+
+    }
+
+
+    public Meeting Get(string meetingId)
+    {
+      return this.Get(new ObjectId(meetingId));
+
+    }
+
+    /// <summary>
+    /// Get all Retrospective objects for a given team
+    /// </summary>
+    /// <param name="teamObjectId"></param>
+    /// <returns></returns>
+    public Meeting Get(ObjectId retroId)
+    {
+      var sessions = database.MongoDatabase.GetCollection<Meeting>(collection);
+      var query = from session in sessions.AsQueryable<Meeting>()
+                  where session.Id == retroId
+                  select session;
+
+      return query.FirstOrDefault();
+
+    }
+  }
 
 }
