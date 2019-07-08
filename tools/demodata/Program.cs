3c52fa49f3e51a16a185f170184587716d818fa2
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2019-07-07 17:12:13 -0700

    fix compilation errors and add logging to demodata program

--- tools/demodata/Program.cs
+++ tools/demodata/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 using Retrospective.Data;
 
@@ -8,31 +10,56 @@ namespace demodata
   {
     static int Main(string[] args)
     {
-      var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTIONSTRING");
-      var databaseName = Environment.GetEnvironmentVariable("DB_NAME");
 
 
-      if (String.IsNullOrWhiteSpace(connectionString))
+      var serviceCollection = new ServiceCollection();
+      ConfigureServices(serviceCollection);
+      var serviceProvider = serviceCollection.BuildServiceProvider();
+
+      var logger = serviceProvider.GetService<ILogger<Program>>();
+
+      try
       {
-        Console.WriteLine("DB_CONNECTIONSTRING environment variable is not set");
-        return -1;
-      }
+        throw new Exception("hey");
 
-      if (String.IsNullOrWhiteSpace(databaseName))
+        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTIONSTRING");
+        var databaseName = Environment.GetEnvironmentVariable("DB_NAME");
+
+
+        if (String.IsNullOrWhiteSpace(connectionString))
+        {
+          Console.WriteLine("DB_CONNECTIONSTRING environment variable is not set");
+          return -1;
+        }
+
+        if (String.IsNullOrWhiteSpace(databaseName))
+        {
+          Console.WriteLine("DB_NAME environment varialbe is not set");
+          return -1;
+        }
+
+        //start with an empty database
+        var client = new MongoClient(connectionString);
+
+        //connect to the database
+        Retrospective.Data.Database database = new Database(databaseName);
+
+        DemoData demoData = new DemoData(database, serviceProvider.GetService<ILogger<DemoData>>());
+        demoData.Initialize();
+        return 0;
+      }
+      catch (Exception ex)
       {
-        Console.WriteLine("DB_NAME environment varialbe is not set");
-        return -1;
+        Console.WriteLine("An error occured.");
+        logger.LogCritical(ex, "An error occured" );
+        return -2;
       }
+    }
 
-      //start with an empty database
-      var client = new MongoClient(connectionString);
-
-      //connect to the database
-      Retrospective.Data.Database database = new Database(databaseName);
-
-      DemoData demoData = new DemoData(database);
-      demoData.Initialize();
-      return 0;
+    private static void ConfigureServices(IServiceCollection services)
+    {
+      services.AddLogging(configure => configure.AddConsole())
+              .Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Information);
     }
   }
 }
