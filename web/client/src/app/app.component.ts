bd6d0a6db7160da2f2a73cca32140f08e5644838
danielj-jordan <12012148+danielj-jordan@users.noreply.github.com>
2019-08-24 13:51:52 -0700

    add google analytics

--- web/client/src/app/app.component.ts
+++ web/client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare let gtag: Function;
 
 
 @Component({
@@ -8,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Agile Retrospective Comments';
+
+  constructor(public router: Router){   
+    this.router.events.subscribe(event => {
+       if(event instanceof NavigationEnd){
+           gtag('config', 'UA-146410514-1', 
+                 {
+                   'page_path': event.urlAfterRedirects
+                 }
+                );
+        }
+     }
+  )}
 }
